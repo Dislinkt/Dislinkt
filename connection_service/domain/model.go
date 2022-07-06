50000d0d65b052fa3460818117a95b3a1e82a75d
VladimirJ123 <jovanovic.ra128.2018@uns.ac.rs>
2022-07-07 00:00:58 +0200

    feat: add field and skill, job offer recommendation

--- connection_service/domain/model.go
+++ connection_service/domain/model.go
@@ -1,5 +1,9 @@
 package domain
 
+import (
+	"google.golang.org/genproto/googleapis/type/date"
+)
+
 type UserNode struct {
 	UserUID string
 	Status  ProfileStatus
@@ -11,3 +15,14 @@ const (
 	Private ProfileStatus = "PRIVATE"
 	Public                = "PUBLIC"
 )
+
+type JobOffer struct {
+	Id            string
+	Position      string
+	Preconditions string
+	DatePosted    date.Date
+	Duration      string
+	Location      string
+	Title         string
+	Field         string
+}
--- connection_service/infrastructure/persistance/connection_neo4j_store.go
+++ connection_service/infrastructure/persistance/connection_neo4j_store.go
@@ -2,6 +2,7 @@ package persistance
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	pb "github.com/dislinkt/common/proto/connection_service"
@@ -633,4 +634,384 @@ func (store *ConnectionDBStore) RecommendUsersByConnection(currentUserUUID strin
 	})
 
 	return users, err
+}
+
+func (store *ConnectionDBStore) unblockConnection(driver neo4j.Driver, currentUser string, blockedUser string) (*pb.BlockedUserStatus, error) {
+	session := (*store.connectionDB).NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer func(session neo4j.Session) {
+		err := session.Close()
+		if err != nil {
+
+		}
+	}(session)
+
+	result, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		if checkIfUserExist(currentUser, tx) && checkIfUserExist(blockedUser, tx) {
+
+			records, err := tx.Run("MATCH (u1:UserNode) WHERE u1.uid = $requestSender MATCH (u2:UserNode) WHERE u2.uid = $requestGet  MATCH (u1)-[r1:CONNECTION]->(u2) MATCH (u2)-[r2:CONNECTION]->(u1) return r1.status, r2.status", map[string]interface{}{
+				"requestSender": currentUser,
+				"requestGet":    blockedUser,
+			})
+
+			if err != nil {
+				return nil, err
+			}
+			record, err := records.Single()
+			if err != nil {
+				return nil, err
+			}
+
+			status1 := record.Values[0].(string)
+			status2 := record.Values[1].(string)
+
+			if status1 == "BLOCK" && status2 == "BLOCKED" {
+				_, err := tx.Run("match (u1:UserNode) where u1.uid = $currentUser "+
+					"match (u2:UserNode) where u2.uid = $blockedUser "+
+					"MATCH (u1)-[r1:CONNECTION {status: 'BLOCK'}]->(u2) "+
+					"MATCH (u2)-[r2:CONNECTION {status: 'BLOCKED'}]->(u1) "+
+					"set r1.status = $status, r2.status = $status", map[string]interface{}{
+					"currentUser": currentUser,
+					"blockedUser": blockedUser,
+					"status":      "CONNECTED",
+				})
+				if err != nil {
+					return nil, err
+				} else {
+					return &pb.BlockedUserStatus{
+						CurrentUserUUID:    currentUser,
+						BlockedUserUUID:    blockedUser,
+						ConnectionResponse: "Connection status - CONNECTED",
+					}, nil
+				}
+			} else if status1 == "BLOCK" && status2 == "BLOCK" {
+
+				_, err := tx.Run("match (u1:UserNode) where u1.uid = $currentUser "+
+					"match (u2:UserNode) where u2.uid = $blockedUser "+
+					"MATCH (u1)-[r1:CONNECTION {status: 'BLOCK'}]->(u2) "+
+					"MATCH (u2)-[r2:CONNECTION {status: 'BLOCK'}]->(u1) "+
+					"set r1.status = $status", map[string]interface{}{
+					"currentUser": currentUser,
+					"blockedUser": blockedUser,
+					"status":      "BLOCKED",
+				})
+				if err != nil {
+					return nil, err
+				} else {
+					return &pb.BlockedUserStatus{
+						CurrentUserUUID:    currentUser,
+						BlockedUserUUID:    blockedUser,
+						ConnectionResponse: "Connection status - BLOCKED",
+					}, nil
+				}
+			} else {
+				return &pb.BlockedUserStatus{
+					CurrentUserUUID:    "",
+					BlockedUserUUID:    "",
+					ConnectionResponse: "Connection refused - user not found",
+				}, nil
+			}
+		} else {
+			return &pb.BlockedUserStatus{
+				CurrentUserUUID:    "",
+				BlockedUserUUID:    "",
+				ConnectionResponse: "Connection refused - user not found",
+			}, nil
+		}
+
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*pb.BlockedUserStatus), nil
+}
+
+func (store *ConnectionDBStore) InsertField(driver neo4j.Driver, name string) (string, error) {
+	session := (*store.connectionDB).NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer func(session neo4j.Session) {
+		err := session.Close()
+		if err != nil {
+
+		}
+	}(session)
+
+	result, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		records, err := tx.Run("CREATE (n:Field { name: $name  }) RETURN n.name", map[string]interface{}{
+			"name": name,
+		})
+
+		if err != nil {
+			return "", err
+		}
+		record, err := records.Single()
+		if err != nil {
+			return "", err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return record.Values[0].(string), nil
+
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.(string), nil
+}
+
+func (store *ConnectionDBStore) InsertSkill(driver neo4j.Driver, name string) (string, error) {
+	session := (*store.connectionDB).NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer func(session neo4j.Session) {
+		err := session.Close()
+		if err != nil {
+
+		}
+	}(session)
+
+	result, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		records, err := tx.Run("CREATE (n:Skill { name: $name  }) RETURN n.name", map[string]interface{}{
+			"name": name,
+		})
+
+		if err != nil {
+			return "", err
+		}
+		record, err := records.Single()
+		if err != nil {
+			return "", err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return record.Values[0].(string), nil
+
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.(string), nil
+}
+
+func (store *ConnectionDBStore) InsertJobOffer(driver neo4j.Driver, jobOffer domain.JobOffer) (string, error) {
+	session := (*store.connectionDB).NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer func(session neo4j.Session) {
+		err := session.Close()
+		if err != nil {
+
+		}
+	}(session)
+
+	result, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+		dateNow := time.Now().Local().Unix()
+		records, err := tx.Run("CREATE (n:JobOffer { Id: $id,Position: $position,DatePosted: $datePosted, Duration: $duration, "+
+			"Location: $location, Title: $title,Field: $field,Preconditions: $preconditions}) RETURN n.Id", map[string]interface{}{
+			"id":            jobOffer.Id,
+			"position":      jobOffer.Position,
+			"datePosted":    dateNow,
+			"duration":      jobOffer.Duration,
+			"location":      jobOffer.Location,
+			"title":         jobOffer.Title,
+			"field":         jobOffer.Field,
+			"preconditions": jobOffer.Preconditions,
+		})
+
+		if err != nil {
+			return "", err
+		}
+
+		records, err = tx.Run("match (j:JobOffer) where j.Id = $id "+
+			"match (f:Field) where f.name = $field "+
+			"CREATE (j)-[r1:FIELDS]->(f)", map[string]interface{}{
+			"id":    jobOffer.Id,
+			"field": jobOffer.Field,
+		})
+
+		s_array := strings.Split(jobOffer.Preconditions, ",")
+
+		for _, s := range s_array {
+
+			records, err = tx.Run("match (j:JobOffer) where j.Id = $id "+
+				"match (s:Skill) where s.name = $skill "+
+				"CREATE (j)-[r1:SKILLS]->(s) return j.Id", map[string]interface{}{
+				"id":    jobOffer.Id,
+				"skill": s,
+			})
+		}
+
+		record, err := records.Single()
+		if err != nil {
+			return "", err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return record.Values[0].(string), nil
+
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.(string), nil
+}
+
+func InsertSkillToUser(driver neo4j.Driver, name string, uuid string) (string, error) {
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	result, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		records, err := tx.Run("match (u1:UserNode) where u1.uid = $uuid "+
+			"match (s:Skill) where s.name = $name "+
+			"CREATE (u1)-[r1:SKILLS]->(s) return u1.uid", map[string]interface{}{
+			"name": name,
+			"uuid": uuid,
+		})
+
+		if err != nil {
+			return "", err
+		}
+		record, err := records.Single()
+		if err != nil {
+			return "", err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return record.Values[0].(string), nil
+
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.(string), nil
+}
+
+func InsertFieldToUser(driver neo4j.Driver, name string, uuid string) (string, error) {
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	result, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		records, err := tx.Run("match (u1:UserNode) where u1.uid = $uuid "+
+			"match (s:Field) where s.name = $name "+
+			"CREATE (u1)-[r1:FIELDS]->(s) return u1.uid", map[string]interface{}{
+			"name": name,
+			"uuid": uuid,
+		})
+
+		if err != nil {
+			return "", err
+		}
+		record, err := records.Single()
+		if err != nil {
+			return "", err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return record.Values[0].(string), nil
+
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.(string), nil
+}
+
+func RecommendJobBySkill(driver neo4j.Driver, userUid string) (jobs []*domain.JobOffer, error) {
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+	_, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		if !checkIfUserExist(userUid, tx) {
+			return nil, nil
+		}
+
+		records, err := tx.Run("match (u1:UserNode) where u1.uid = $uuid "+
+			"match (s:Skill) "+
+			"match (j:JobOffer) "+
+			"MATCH (u1)-[r1:SKILLS]->(s) "+
+			"MATCH (j)-[r2:SKILLS]->(s) "+
+			"return j.Id , j.Position, j.Preconditions,j.DatePosted, j.Duration, j.Location, j.Title, j.Field", map[string]interface{}{
+			"uuid": userUid,
+		})
+
+		for records.Next() {
+
+			node := domain.JobOffer{
+				Id:            records.Record().Values[0].(string),
+				Position:      records.Record().Values[1].(string),
+				Preconditions: records.Record().Values[2].(string),
+				Duration:      records.Record().Values[4].(string),
+				Location:      records.Record().Values[5].(string),
+				Title:         records.Record().Values[6].(string),
+				Field:         records.Record().Values[7].(string),
+			}
+
+			jobs = append(jobs, &node)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+		//re, err := records.Single()
+		if err != nil {
+			return nil, err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return jobs, nil
+	})
+
+	return jobs, err
+}
+
+func RecommendJobByField(driver neo4j.Driver, userUid string) (jobs []*domain.JobOffer, error) {
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+	_, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+
+		if !checkIfUserExist(userUid, tx) {
+			return nil, nil
+		}
+
+		records, err := tx.Run("match (u1:UserNode) where u1.uid = $uuid "+
+			"match (s:Field) "+
+			"match (j:JobOffer) "+
+			"MATCH (u1)-[r1:FIELDS]->(s) "+
+			"MATCH (j)-[r2:FIELDS]->(s) "+
+			"return j.Id , j.Position, j.Preconditions,j.DatePosted, j.Duration, j.Location, j.Title, j.Field", map[string]interface{}{
+			"uuid": userUid,
+		})
+
+		for records.Next() {
+
+			node := domain.JobOffer{
+				Id:            records.Record().Values[0].(string),
+				Position:      records.Record().Values[1].(string),
+				Preconditions: records.Record().Values[2].(string),
+				Duration:      records.Record().Values[4].(string),
+				Location:      records.Record().Values[5].(string),
+				Title:         records.Record().Values[6].(string),
+				Field:         records.Record().Values[7].(string),
+			}
+
+			jobs = append(jobs, &node)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+		//re, err := records.Single()
+		if err != nil {
+			return nil, err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return jobs, nil
+	})
+
+	return jobs, err
 }
